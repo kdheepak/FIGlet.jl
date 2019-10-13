4c6f3ef754c34b0f85763e3174ae0d8a3c4f819b
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-13 01:14:45 -0600

    Remove docstring

--- src/FIGlet.jl
+++ src/FIGlet.jl
@@ -60,25 +60,6 @@ Base.@enum(Layout,
 )
 
 
-raw"""
-THE HEADER LINE
-
-The header line gives information about the FIGfont.  Here is an example
-showing the names of all parameters:
-
-          flf2a$ 6 5 20 15 3 0 143 229    NOTE: The first five characters in
-            |  | | | |  |  | |  |   |     the entire file must be "flf2a".
-           /  /  | | |  |  | |  |   \
-  Signature  /  /  | |  |  | |   \   Codetag_Count
-    Hardblank  /  /  |  |  |  \   Full_Layout*
-         Height  /   |  |   \  Print_Direction
-         Baseline   /    \   Comment_Lines
-          Max_Length      Old_Layout*
-
-  * The two layout parameters are closely related and fairly complex.
-      (See "INTERPRETATION OF LAYOUT PARAMETERS".)
-
-"""
 struct FIGletHeader
     hardblank::Char
     height::Int
