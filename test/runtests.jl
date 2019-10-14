a5a966cfa84a7f413379810dcedaa575c002c72e
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-13 21:37:53 -0600

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
     iob = IOBuffer(b"flf2a", read=true);
     @test FIGlet.readmagic(iob) == UInt8['f', 'l', 'f', '2', 'a']
 
-    @test FIGlet.FIGletHeader('$', 6, 5, 16, 15, 11) == FIGlet.FIGletHeader('$', 6, 5, 16, 15, 11, 0, 2, 0)
+    @test FIGlet.FIGletHeader('$', 6, 5, 16, 15, 11) == FIGlet.FIGletHeader('$', 6, 5, 16, 15, 11, 0, 143, 0)
 
     @test FIGlet.availablefonts() |> length == 680
 
